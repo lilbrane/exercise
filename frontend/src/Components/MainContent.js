0d976881a42822d69b87bd6a6cd35ec54deb0b02
Vid Beranic <vidberanic@gmail.com>
2024-11-01 13:25:51 +0100

    valid input checking on editUser, gender now radiobutton

--- frontend/src/Components/MainContent.js
+++ frontend/src/Components/MainContent.js
@@ -6,14 +6,20 @@ const MainContent = ({user}) => {
     const [editing, setEditing] = useState(false);
     const [currentUser, setCurrentUser] = useState(user);
     const [editHover, setEditHover] = useState(false)
+    const [emailErr, setEmailErr] = useState("")
 
     useEffect(() => {
         setCurrentUser(user);
         setEditing(false);
+        setEmailErr("")
 
     }, [user]);
 
+    
+
     const saveChanges = async() => {
+        if(emailErr != "") return
+
         try {
             const response = await axios.put(`https://dummyjson.com/users/${currentUser.id}`, {...currentUser})
             
@@ -24,6 +30,19 @@ const MainContent = ({user}) => {
             console.error(`ERROR in updating user - ${err.message}`)
         }
     }
+
+    const isValidEmail = (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+
+
+    const checkEmail = () => {
+
+        if (!isValidEmail(currentUser.email)) {
+            setEmailErr("Please enter a valid email address.");
+        } else {
+            setEmailErr("");
+        }
+    };
+
   return (
     
     <div className='w-3/4 h-screen p-4 border-r text-syyclopsBlue overflow-y-auto flex'>
@@ -77,28 +96,76 @@ const MainContent = ({user}) => {
 
                             <p className='col-span-1 p-2'>Age:</p>
                             {editing ? (
-                                <input className='border rounded border-syyclopsLightBlue border-opacity-30 p-2 col-span-2' value={currentUser.age} onChange={(e) => setCurrentUser({ ...currentUser, age: e.target.value })}/>
+                                <input className='border rounded border-syyclopsLightBlue border-opacity-30 p-2 col-span-2' value={currentUser.age} 
+                                onChange={(e) => {
+                                    // check if input is positive number between 0 and 120 (lower limit could be raised in irl scenario) 
+                                    const newAge = e.target.value;
+                                    if ((/^[0-9]+$/.test(newAge) && Number(newAge) > 0 && Number(newAge) <= 120) || newAge === "")
+                                        setCurrentUser({ ...currentUser, age: newAge });
+                                }
+                                    
+                                }/>
                             ) : (
                                 <span className='p-2 col-span-2'>{currentUser.age}</span>
                             )}
 
                             <p className='col-span-1 p-2'>Gender:</p>
                             {editing ? (
-                                <input className='border rounded border-syyclopsLightBlue border-opacity-30 p-2 col-span-2' value={currentUser.gender} onChange={(e) => setCurrentUser({ ...currentUser, gender: e.target.value })} />
+                                <div className='col-span-2 flex justify-between mx-10 overflow-hidden'>
+                                    <div>
+                                        <input type="radio" id="male" name="gender" value="male" 
+                                            checked={currentUser.gender === "male"} 
+                                            onChange={(e) => setCurrentUser({ ...currentUser, gender: e.target.value })}
+                                            />
+                                        <label htmlFor="male">male</label>
+                                    </div>
+
+                                    <div>
+                                        <input type="radio" id="female" name="gender" value="female" 
+                                            checked={currentUser.gender === "female"}
+                                            onChange={(e) => setCurrentUser({ ...currentUser, gender: e.target.value})}
+                                            />
+                                        <label htmlFor="female">female</label>
+                                    </div>
+
+                                    <div>
+                                        <input type="radio" id="other" name="gender" value="other" 
+                                            checked={currentUser.gender === "other"} 
+                                            onChange={(e) => setCurrentUser({ ...currentUser, gender: e.target.value})}
+                                            />
+                                        <label htmlFor="other">other</label>
+                                    </div>
+                                </div>
                             ) : (
                                 <span className='p-2 col-span-2'>{currentUser.gender}</span>
                             )}
+                             {/* <input  className='border rounded border-syyclopsLightBlue border-opacity-30 p-2 col-span-2' value={currentUser.gender} onChange={(e) => setCurrentUser({ ...currentUser, gender: e.target.value })} /> */}
 
                             <p className='col-span-1 p-2'>Email:</p>
                             {editing ? (
-                                <input className='border rounded border-syyclopsLightBlue border-opacity-30 p-2 col-span-2' value={currentUser.email} onChange={(e) => setCurrentUser({ ...currentUser, email: e.target.value })} />
+                                <div className='col-span-2'>
+                                    <input className='border rounded border-syyclopsLightBlue border-opacity-30 p-2 ' value={currentUser.email} 
+                                    onChange={(e) => setCurrentUser({ ...currentUser, email: e.target.value })} 
+                                    onBlur={() => checkEmail()}
+                                    />
+                                    {emailErr !== "" && <p className="text-red-500 text-lg">{emailErr}</p>}
+
+                                </div>
+
+
                             ) : (
                                 <span className='p-2 col-span-2 whitespace-nowrap overflow-hidden'>{currentUser.email}</span>
                             )}
 
                             <p className='col-span-1 p-2'>Phone number:</p>
                             {editing ? (
-                                <input className='border rounded border-syyclopsLightBlue border-opacity-30 p-2 col-span-2' value={currentUser.phone} onChange={(e) => setCurrentUser({ ...currentUser, phone: e.target.value })} />
+                                <input className='border rounded border-syyclopsLightBlue border-opacity-30 p-2 col-span-2' value={currentUser.phone} 
+                                onChange={(e) => {
+                                    // regex for phone number, allowed + in front, and dashes and spaces in middle
+                                    const newPhoneNum = e.target.value;
+                                    if (/^\+?[0-9\s()-]*$/.test(newPhoneNum) || newPhoneNum === "")
+                                        setCurrentUser({ ...currentUser, phone: newPhoneNum });
+                                }} />
                             ) : (
                                 <span className='p-2 col-span-2 whitespace-nowrap overflow-hidden'>{currentUser.phone}</span>
                             )}
