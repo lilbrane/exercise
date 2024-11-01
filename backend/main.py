2af1ae1dae8ae77d24fdf69773c75cc56f4765c8
Vid Beranic <vidberanic@gmail.com>
2024-11-01 18:40:53 +0100

    GET and PUT, users.py filled with more users

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,46 @@
-from typing import Union
+from typing import Union,List, Optional
+from pydantic import BaseModel
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
+import users
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class User(BaseModel):
+    id: int
+    firstName: str
+    lastName: str
+    age: int
+    gender: str
+    email: str
+    phone: str
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+# get limit from query
 @app.get("/users")
-def get_users():
-    # TODO ASSIGNMENT: Return a list of users
-    return []
+def get_users(limit: Optional[int] = 20):
+    return {"users": users.get_users(limit)}
+
+@app.put("/users/{user_id}")
+def update_user(user_id: str, updated_user: User):
+
+    for user in users.get_users():
+        if int(user_id) == user["id"]:
+            user = updated_user
+            return user
+
+    raise HTTPException(status_code=404, detail="User not found")
--- backend/users.py
+++ backend/users.py
@@ -1,4 +1,4 @@
-def get_users():
+def get_users(limit=20):
     return [
         {
             "id": 1,
@@ -44,5 +44,186 @@ def get_users():
             "gender": "male",
             "email": "charlie@doe.com",
             "phone": "123-456-7890",
+        },
+        {
+            "id": 6,
+            "firstName": "Emily",
+            "lastName": "Johnson",
+            "age": 28,
+            "gender": "female",
+            "email": "emily.johnson@x.dummyjson.com",
+            "phone": "+81 965-431-3024"
+        },
+        {
+            "id": 7,
+            "firstName": "Michael",
+            "lastName": "Williams",
+            "age": 35,
+            "gender": "male",
+            "email": "michael.williams@x.dummyjson.com",
+            "phone": "+49 258-627-6644"
+        },
+        {
+            "id": 8,
+            "firstName": "Sophia",
+            "lastName": "Brown",
+            "age": 42,
+            "gender": "female",
+            "email": "sophia.brown@x.dummyjson.com",
+            "phone": "+81 210-652-2785"
+        },
+        {
+            "id": 9,
+            "firstName": "James",
+            "lastName": "Davis",
+            "age": 45,
+            "gender": "male",
+            "email": "james.davis@x.dummyjson.com",
+            "phone": "+49 614-958-9364"
+        },
+        {
+            "id": 10,
+            "firstName": "Emma",
+            "lastName": "Miller",
+            "age": 30,
+            "gender": "female",
+            "email": "emma.miller@x.dummyjson.com",
+            "phone": "+91 759-776-1614"
+        },
+        {
+            "id": 11,
+            "firstName": "Olivia",
+            "lastName": "Wilson",
+            "age": 22,
+            "gender": "female",
+            "email": "olivia.wilson@x.dummyjson.com",
+            "phone": "+91 607-295-6448"
+        },
+        {
+            "id": 12,
+            "firstName": "Alexander",
+            "lastName": "Jones",
+            "age": 38,
+            "gender": "male",
+            "email": "alexander.jones@x.dummyjson.com",
+            "phone": "+61 260-824-4986"
+        },
+        {
+            "id": 13,
+            "firstName": "Ava",
+            "lastName": "Taylor",
+            "age": 27,
+            "gender": "female",
+            "email": "ava.taylor@x.dummyjson.com",
+            "phone": "+1 458-853-7877"
+        },
+        {
+            "id": 14,
+            "firstName": "Ethan",
+            "lastName": "Martinez",
+            "age": 33,
+            "gender": "male",
+            "email": "ethan.martinez@x.dummyjson.com",
+            "phone": "+92 933-608-5081"
+        },
+        {
+            "id": 15,
+            "firstName": "Isabella",
+            "lastName": "Anderson",
+            "age": 31,
+            "gender": "female",
+            "email": "isabella.anderson@x.dummyjson.com",
+            "phone": "+49 770-658-4885"
+        },
+        {
+            "id": 16,
+            "firstName": "Liam",
+            "lastName": "Garcia",
+            "age": 29,
+            "gender": "male",
+            "email": "liam.garcia@x.dummyjson.com",
+            "phone": "+92 870-217-6201"
+        },
+        {
+            "id": 17,
+            "firstName": "Mia",
+            "lastName": "Rodriguez",
+            "age": 24,
+            "gender": "female",
+            "email": "mia.rodriguez@x.dummyjson.com",
+            "phone": "+49 989-461-8403"
+        },
+        {
+            "id": 18,
+            "firstName": "Noah",
+            "lastName": "Hernandez",
+            "age": 40,
+            "gender": "male",
+            "email": "noah.hernandez@x.dummyjson.com",
+            "phone": "+49 393-605-6968"
+        },
+        {
+            "id": 19,
+            "firstName": "Charlotte",
+            "lastName": "Lopez",
+            "age": 36,
+            "gender": "female",
+            "email": "charlotte.lopez@x.dummyjson.com",
+            "phone": "+44 373-953-5028"
+        },
+        {
+            "id": 20,
+            "firstName": "William",
+            "lastName": "Gonzalez",
+            "age": 32,
+            "gender": "male",
+            "email": "william.gonzalez@x.dummyjson.com",
+            "phone": "+81 905-252-7319"
+        },
+        {
+            "id": 21,
+            "firstName": "Avery",
+            "lastName": "Perez",
+            "age": 25,
+            "gender": "female",
+            "email": "avery.perez@x.dummyjson.com",
+            "phone": "+61 731-431-3457"
+        },
+        {
+            "id": 22,
+            "firstName": "Evelyn",
+            "lastName": "Sanchez",
+            "age": 37,
+            "gender": "female",
+            "email": "evelyn.sanchez@x.dummyjson.com",
+            "phone": "+1 623-880-6871"
+        },
+        {
+            "id": 23,
+            "firstName": "Logan",
+            "lastName": "Torres",
+            "age": 31,
+            "gender": "male",
+            "email": "logan.torres@x.dummyjson.com",
+            "phone": "+81 507-434-8733"
+        },
+        {
+            "id": 24,
+            "firstName": "Abigail",
+            "lastName": "Rivera",
+            "age": 28,
+            "gender": "female",
+            "email": "abigail.rivera@x.dummyjson.com",
+            "phone": "+91 228-363-7806"
+        },
+        {
+            "id": 25,
+            "firstName": "Jackson",
+            "lastName": "Evans",
+            "age": 34,
+            "gender": "male",
+            "email": "jackson.evans@x.dummyjson.com",
+            "phone": "+44 468-628-6686"
         }
-    ]
+    ][:limit]
+    
