d38f7654b34c9d1bb98e5f67dfa4a117d109e65e
Vid Beranic <vidberanic@gmail.com>
2024-11-01 12:45:28 +0100

    Added sticky logo to sidebar

--- frontend/src/Components/Sidebar.js
+++ frontend/src/Components/Sidebar.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from "axios"
+import logo from "../syyclopsLogo.png"
 
 const Sidebar = ({ setSelected }) => {
     const [users, setUsers] = useState([]);
@@ -20,42 +21,43 @@ const Sidebar = ({ setSelected }) => {
         fetchUsers();
     }, [])
 
-    const select = (user) => {
-        console.log(user)
-        setSelected(user)
-    }
-
   return (
-    <div className='sidebar w-1/4 h-screen p-4 border-r bg-syyclopsBlue text-white overflow-y-auto'>
-        <p className='text-syyclopsOrange font-semibold my-6 mb-10 text-4xl'>Users</p>
-        <div>
-            {
-                users.map((user) => (
-                    <div key={user.id} className='userDiv border-syyclopsOrange rounded-xl border p-4 text-2xl my-4 hover:bg-syyclopsLightBlue transition duration-300 whitespace-nowrap overflow-hidden' onClick={()=> {select(user)}}>
-                    {/* <div key={user.id} className='userDiv rounded-xl p-4 text-2xl my-4 hover:bg-syyclopsLightBlue ' onClick={()=> {select(user)}}> */}
-                    <div className='mainInfo flex w-full font-semibold'>
-                        <p className='mr-4'>{user.firstName}</p>
-                        <p>{user.lastName}</p>
-                        <div className='mr-5 ml-auto'>
-                            <p>{user.id}</p>
-                        </div>
-                    </div>
-                    
-                    {/* <div className='secondInfo flex mt-2 text-base text-gray-400'>
-                        <p className='mr-4'>Age: {user.age}</p>
-                        <p>Gender: {user.gender}</p>
-                    </div>
-    
-                    <div className='contactInfo flex mt-3 space-x-6 text-lg'>
-                        <p>{user.email}</p>
-                        <p>{user.phone}</p>
-                    </div> */}
-    
-                </div>
-                ))
-            }
-           
+    <div className='sidebar w-1/4 h-screen  border-r bg-syyclopsBlue text-white overflow-y-auto '>
+        <div className='flex justify-center sticky top-0 bg-syyclopsBlue p-4 mt-0 '>
+            <img src={logo} alt="logo" className=' my-auto w-1/2' />
         </div>
+        <div className='px-4'>
+
+            <p className='text-syyclopsOrange font-semibold  text-3xl'>Users</p>
+            <div>
+                {
+                    users.map((user) => (
+                        <div key={user.id} className='userDiv border-syyclopsOrange rounded-xl border p-4 text-2xl my-4 hover:bg-syyclopsLightBlue transition duration-300 whitespace-nowrap overflow-hidden' onClick={()=> {setSelected(user)}}>
+                        <div className='mainInfo flex w-full font-semibold'>
+                            <p className='mr-4'>{user.firstName}</p>
+                            <p>{user.lastName}</p>
+                            <div className='mr-5 ml-auto'>
+                                <p>{user.id}</p>
+                            </div>
+                        </div>
+                        
+                        {/* <div className='secondInfo flex mt-2 text-base text-gray-400'>
+                            <p className='mr-4'>Age: {user.age}</p>
+                            <p>Gender: {user.gender}</p>
+                        </div>
+        
+                        <div className='contactInfo flex mt-3 space-x-6 text-lg'>
+                            <p>{user.email}</p>
+                            <p>{user.phone}</p>
+                        </div> */}
+        
+                    </div>
+                    ))
+                }
+            
+            </div>
+        </div>
+    
     </div>
   )
 }
